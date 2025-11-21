9f5cac4427a3f341e73cfe22f482152ea3225693
Allan22995 <153982015+Allan22995@users.noreply.github.com>
2025-11-21 01:56:07 -0300

    feat: Declare asset types for Vite
    Allows Vite to process and import various asset file types, including CSS, PNG, SVG, JPEG, and JPG. This improves the build process and enables direct usage of these assets in the application.

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,4 +1,8 @@
-/// <reference types="vite/client" />
+declare module '*.css';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.jpeg';
+declare module '*.jpg';
 
 interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string
